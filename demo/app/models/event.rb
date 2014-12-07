ced67a4488590241dd808b092faa195104c1b864
Yin Zhang <fzzying@outlook.com>
2014-12-07 23:38:42 +0000

    Active Record and Restful

--- demo/app/models/event.rb
+++ demo/app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ActiveRecord::Base
+  belongs_to :category
+  has_many :attendees
+  has_many :event_groupships
+  has_many :groups, :through => :event_groupships
+  has_one :location
   validates_presence_of :name
+  delegate :name, :to => :category, :prefix => true, :allow_nil => true
 end
