31ef6f76a6a34b4282cc5a4a0d1819586362f6bb
Ubuntu <ubuntu@ip-172-31-43-197.us-west-2.compute.internal>
2014-12-07 00:23:01 +0000

    scaffold CRUD

---
+++ demo/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :people
+
+  get "welcome/say_hello" => "welcome#say"
+  get "welcome" => "welcome#index"
+  root :to => "welcome#index"
+  match ':controller(/:action(/:id(.:format)))', :via => :all
+end
