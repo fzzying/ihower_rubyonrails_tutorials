6f658fe56a4efba92acd203080161028eaca13cc
Ubuntu <ubuntu@ip-172-31-43-197.us-west-2.compute.internal>
2014-12-07 19:39:11 +0000

    RESTFUL Transform

--- demo/app/controllers/events_controller.rb
+++ demo/app/controllers/events_controller.rb
@@ -3,6 +3,13 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.page(params[:page]).per(5)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml {render :xml => @events.to_xml }
+      format.json {render :json => @events.to_json }
+      format.atom { @feed_title = "My event list" } #index.atom.builder
+    end
   end
 
   def new
@@ -13,14 +20,19 @@ class EventsController < ApplicationController
     @event = Event.new(event_params)
     if @event.save
       flash[:notice] = "event was successfully created"
-      redirect_to :action => :index
+      redirect_to events_url
     else
       render :action => :new
     end
   end
 
   def show
-    @page_title = @event.name
+    @event = Event.find(params[:id])
+    respond_to do |format|
+      format.html { @page_title = @event.name } # show.html.erb
+      format.xml # show.xml.builder
+      format.json { render :json => { id: @event.id, name: @event.name }.to_json }
+    end
   end
 
   def edit
@@ -29,7 +41,7 @@ class EventsController < ApplicationController
   def update
     if @event.update(event_params)
       flash[:notice] = "event was successfully updated"
-      redirect_to :action => :show, :id => @event
+      redirect_to event_url(@event)
     else
       render :action => :edit
     end
--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :people
+  resources :events
 
   get "welcome/say_hello" => "welcome#say"
   get "welcome" => "welcome#index"
   root :to => "welcome#index"
-  match ':controller(/:action(/:id(.:format)))', :via => :all
 end
